f51a24694cacfe37e5219c46cda36405ccbbe058
John Morris <john@zultron.com>
2025-06-17 12:38:03 -0500

    install_scripts:  Move YDLidar SDK to later Docker build stage
    Move from `base` buld stage, which installs only very generic stuff
    not related to ROS, to `ros_custom` build stage, where we already have
    significant build environment infrastructure installed, and where
    other dependencies of this repo are installed.

--- install_scripts/docker/ros_custom/2-install-deps.sh
+++ install_scripts/docker/ros_custom/2-install-deps.sh
@@ -14,3 +14,30 @@ cd ${ROS_CUSTOM_SCRIPTS_DIR}
 
 # Compute and install ROS workspace package dependencies
 ${ROS_CUSTOM_SCRIPTS_DIR}/run_rosdep.sh
+
+###########################
+# YDLIDAR SDK
+###########################
+
+# Clone the YDLidar-SDK repository
+# FIXME Build broken after upstream changes
+# https://github.com/YDLIDAR/YDLidar-SDK/issues/58
+# https://github.com/YDLIDAR/YDLidar-SDK/pull/59
+# git clone https://github.com/YDLIDAR/YDLidar-SDK.git
+git clone https://github.com/hannesduske/YDLidar-SDK.git
+cd YDLidar-SDK
+
+# Create build directory
+mkdir -p build
+cd build
+
+# Run cmake with installation prefix set to /usr/local
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..
+
+# Compile
+make
+
+# Install the library
+make install # ...where we can use it now...
+DESTDIR=/root/ros_catkin_ws/ros-export \
+    make install # ...and where it can be copied to final image
